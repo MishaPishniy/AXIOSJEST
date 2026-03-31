00e565249764ee7a634984080cb2604fb03a80b5
Твоё Имя <your@email.com>
2026-03-31 20:32:01 +0300

    new commit actions

--- src/data/postData.js
+++ src/data/postData.js
@@ -3,3 +3,11 @@ export const newPost = {
   body: 'This is a test body',
   userId: 1
 };
+
+
+  export const updatePostBody = {
+      id: 1,
+      title: 'My test post update',
+      body: 'This is a test body',
+      userId: 1
+    };
